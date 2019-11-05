fdc0646b1df53313c86c68f796eda0554d2a7668
cheikh sadibou <cheikht.traore@randstad.ca>
2019-11-04 21:32:51 -0500

    Add login feature

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using NG_Core_Auth.Helpers;
 using NG_Core_Auth.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -18,12 +24,16 @@ namespace NG_Core_Auth.Controllers
 
         private readonly SignInManager<IdentityUser> _signManager;
 
-        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signManager)
+        private readonly AppSettings _appSettings;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signManager,IOptions<AppSettings> appSettings)
         {
             _userManager = userManager;
             _signManager = signManager;
+            _appSettings = appSettings.Value;
         }
 
+        [HttpPost("action")]
         public async  Task<IActionResult> Register([FromBody] RegisterVM formdata)
         {
             //Will hold all th errors related to registration
@@ -60,7 +70,54 @@ namespace NG_Core_Auth.Controllers
         }
 
         //Login
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginVM formdata)
+        {
+            //get the user from the database
+            var user = await _userManager.FindByNameAsync(formdata.Username);
 
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appSettings.Secret));
+
+            Double tokenExpireTime = Convert.ToDouble(_appSettings.ExpireTime);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, formdata.Password))
+            {
+                //Confirmatio email
+                var tokenHandler = new JwtSecurityTokenHandler();
+
+                var tokenDescriptor = new SecurityTokenDescriptor {
+                    Subject = new ClaimsIdentity(new Claim[] {
+                        new Claim(JwtRegisteredClaimNames.Sub, formdata.Username),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(ClaimTypes.NameIdentifier, user.Id),
+                        new Claim(ClaimTypes.Role, roles.FirstOrDefault()),
+                        new Claim("LoggedOn", DateTime.Now.ToString()),
+                    }),
+
+                    SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature),
+                    Issuer = _appSettings.Site,
+                    Audience = _appSettings.Audience,
+                    Expires = DateTime.UtcNow.AddMinutes(tokenExpireTime),
+                };
+
+                //Generate token
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                return Ok(new { token = tokenHandler.WriteToken(token), expiration = token.ValidTo, username = user.UserName, userRole = roles.FirstOrDefault()});
+
+            }
+
+            //return error
+            ModelState.AddModelError("","Username/Password was not found");
+            return Unauthorized(new { LoginError = "Please check the Login Credentials - Invalid Username/Password was entered" });
+        }
+
+        private IActionResult Unauthorized(object p)
+        {
+            throw new NotImplementedException();
+        }
 
 
         //public int MyProperty { get; set; }
